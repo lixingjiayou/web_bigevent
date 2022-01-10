3a887891f5aba32b37a62c265a8340139ec9b779
lixing <907143001@qq.com>
2022-01-10 15:38:38 +0800

    完成个人中心的开发

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -1,57 +1,57 @@
 $(function() {
-  var form = layui.form
-  var layer = layui.layer
-
-  form.verify({
-    nickname: function(value) {
-      if (value.length > 6) {
-        return '昵称长度必须在 1 ~ 6 个字符之间！'
-      }
-    }
-  })
-
-  initUserInfo()
-
-  // 初始化用户的基本信息
-  function initUserInfo() {
-    $.ajax({
-      method: 'GET',
-      url: '/my/userinfo',
-      success: function(res) {
-        if (res.status !== 0) {
-          return layer.msg('获取用户信息失败！')
+    var form = layui.form
+    var layer = layui.layer
+        // 表单验证规则
+    form.verify({
+        nickname: function(value) {
+            if (value.length > 6) {
+                return '昵称长度必须在 1 ~ 6 个字符之间！'
+            }
         }
-        // console.log(res)
-        // 调用 form.val() 快速为表单赋值
-        form.val('formUserInfo', res.data)
-      }
     })
-  }
-
-  // 重置表单的数据
-  $('#btnReset').on('click', function(e) {
-    // 阻止表单的默认重置行为
-    e.preventDefault()
     initUserInfo()
-  })
+        // 初始化用户表单的基本信息
+    function initUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取用户信息失败！')
+                }
+                console.log(res)
+                    // 调用form .val()快速赋值
+                form.val('formUserInfo', res.data)
+            }
+        })
+    }
 
-  // 监听表单的提交事件
-  $('.layui-form').on('submit', function(e) {
-    // 阻止表单的默认提交行为
-    e.preventDefault()
-    // 发起 ajax 数据请求
-    $.ajax({
-      method: 'POST',
-      url: '/my/userinfo',
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
-          return layer.msg('更新用户信息失败！')
-        }
-        layer.msg('更新用户信息成功！')
-        // 调用父页面中的方法，重新渲染用户的头像和用户的信息
-        window.parent.getUserInfo()
-      }
+    // 重置表单数据 submit 提交
+    $('#btnReset').on('click', function(e) {
+        e.preventDefault()
+        initUserInfo()
     })
-  })
+
+    // 监听表单的提交事件 捕获 冒泡
+    $('.layui-form').on('submit', function(e) {
+        e.preventDefault()
+            // 发起ajax数据请求
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            // 参数
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('更新用户信息失败')
+                }
+                layer.msg('更新用户信息成功！')
+                    // 成功后执行 后面的程序
+                    // 调用父页面里面的方法
+                    // window.parent 当前页面的父亲
+                window.parent.getUserInfo()
+            }
+        })
+    })
+
 })
--- assets/js/user/user_pwd.js
+++ assets/js/user/user_pwd.js
@@ -1,34 +1,42 @@
 $(function() {
-  var form = layui.form
-
-  form.verify({
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    samePwd: function(value) {
-      if (value === $('[name=oldPwd]').val()) {
-        return '新旧密码不能相同！'
-      }
-    },
-    rePwd: function(value) {
-      if (value !== $('[name=newPwd]').val()) {
-        return '两次密码不一致！'
-      }
-    }
-  })
-
-  $('.layui-form').on('submit', function(e) {
-    e.preventDefault()
-    $.ajax({
-      method: 'POST',
-      url: '/my/updatepwd',
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
-          return layui.layer.msg('更新密码失败！')
+    // 密码的校验规则
+    var form = layui.form
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位且不能出现空格'],
+        samePwd: function(value) {
+            if (value === $('[name=oldPwd]').val()) {
+                return '新旧密码不能相同！'
+            }
+        },
+        rePwd: function(value) {
+            if (value !== $('[name=newPwd]').val()) {
+                return '两次密码不一致'
+            }
         }
-        layui.layer.msg('更新密码成功！')
-        // 重置表单
-        $('.layui-form')[0].reset()
-      }
     })
-  })
+
+    // 监听提交事件
+    $('.layui-form').on('submit', function(e) {
+        e.preventDefault();
+        // 点击了提交按钮发起ajax请求
+        $.ajax({
+            method: 'POST',
+            url: '/my/updatepwd',
+            // 快速获取表单数据
+            data: $(this).serialize(),
+            success: function(res) {
+                console.log(res)
+                if (res.status !== 0) {
+                    return layui.layer.msg('更新密码失败！')
+
+                }
+
+                layui.layer.msg('更新密码成功！')
+                    // $('.layui-form') 是 jquery 元素
+                    // dom 元素 reset() 方法清空 重置表单
+                $('.layui-form')[0].reset()
+            }
+        })
+    })
+
 })
